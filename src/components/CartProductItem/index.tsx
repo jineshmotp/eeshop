24f1145a5f5925798d5812255d573ee9f8a37512
maniyanthottiljinesh <jineshmotp@outlook.com>
2022-07-12 14:39:54 +0200

    thirdv last

--- src/components/CartProductItem/index.tsx
+++ src/components/CartProductItem/index.tsx
@@ -1,72 +1,66 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text,View,Image } from 'react-native';
-
-import { Colors } from 'react-native/Libraries/NewAppScreen';
-
+import {Image, View, Text} from 'react-native';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
-import styles from './styles';
 import QuantitySelector from '../QuantitySelector';
+import styles from './styles';
+import {DataStore} from 'aws-amplify';
 
-interface CartProductItemProps{
-  cartItem:{
-    id:number;
-    quantity:number;
-    option?:string;
-  item:{
-    id: string;
-    title: string; 
-    image : string;  
-    avgRating:number;
-    ratings: number;
-    price: number;
-    oldPrice?: number;
+import {CartProduct} from '../../models';
+
+interface CartProductItemProps {
+  cartItem: CartProduct;
+}
+
+const CartProductItem = ({cartItem}: CartProductItemProps) => {
+  console.log(cartItem);
+  const {product, ...cartProduct} = cartItem;
+
+  const updateQuantity = async (newQuantity: number) => {
+    const original = await DataStore.query(CartProduct, cartProduct.id);
+
+    await DataStore.save(
+      CartProduct.copyOf(original, updated => {
+        updated.quantity = newQuantity;
+      }),
+    );
   };
-}
-}
 
-const CartProductItem = ({cartItem } : CartProductItemProps) => {
- const {quantity:quantityProp, item } = cartItem;
-
- const [quantity, setQuantity ] = useState(quantityProp);
- 
   return (
     <View style={styles.root}>
-    <View style={styles.row}>     
-            <Image style={styles.image} source={{uri : item.image }} />
-      
-        <View style={styles.rightContainer}>     
-          <Text style={styles.title} numberOfLines={3}>{item.title}</Text>
-          <View style={styles.ratingsContainer}>
-            {[0,0,0,0,0].map((el,i) =>             
-            <FontAwesome   
-            key= { `${item.id}-${i}` }          
-            style={styles.star} 
-            name={ i < Math.floor(item.avgRating) ? 'star' : 'star-o' } 
-            size={18} 
-            color="#e47911" 
-            />
-           
-            )}
-            <Text>{item.ratings}</Text>
-          </View>
-          <Text style={styles.price}>${item.price}
-          {item.oldPrice && (
-          <Text style={styles.oldprice}>          
-          ${item.oldPrice}</Text>
-          )}
+      <View style={styles.row}>
+        <Image style={styles.image} source={{uri: product.image}} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title} numberOfLines={3}>
+            {product.title}
           </Text>
+          {/* Ratings */}
+          <View style={styles.ratingsContainer}>
+            {[0, 0, 0, 0, 0].map((el, i) => (
+              <FontAwesome
+                key={`${product.id}-${i}`}
+                style={styles.star}
+                name={i < Math.floor(product.avgRating) ? 'star' : 'star-o'}
+                size={18}
+                color={'#e47911'}
+              />
+            ))}
+            <Text>{product.ratings}</Text>
           </View>
+          <Text style={styles.price}>
+            from ${product.price}
+            {product.oldPrice && (
+              <Text style={styles.oldPrice}> ${product.oldPrice}</Text>
+            )}
+          </Text>
         </View>
-        <View style={styles.quantityContainer}>
+      </View>
+      <View style={styles.quantityContainer}>
         <QuantitySelector
-          quantity={quantity}
-          setQuantity={setQuantity}
+          quantity={cartProduct.quantity}
+          setQuantity={updateQuantity}
         />
       </View>
-
-      
-      </View>
+    </View>
   );
 };
 
